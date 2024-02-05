288006503258ff8b103f11a005c25a416dd81771
n0tea <artnotea04@gmail.com>
2024-02-06 02:21:55 +0300

    menu start

---
+++ Assets/Scripts/ButtonsScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonsScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void StartGame()
+    {
+        Debug.Log("Нажалось");
+        SceneManager.LoadScene("AstroidMove");
+    }
+    public void CloseApplication()
+    {
+        Application.Quit();
+    }
+}
